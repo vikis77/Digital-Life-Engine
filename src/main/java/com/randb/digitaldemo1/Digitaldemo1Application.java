8a6a74d2875f326683c20f6e64e2ccf2199dce9f
QIN <qin2607994895@gmail.com>
2025-08-22 02:11:27 +0800

    first commit

---
+++ src/main/java/com/randb/digitaldemo1/Digitaldemo1Application.java
@@ -0,0 +1,13 @@
+package com.randb.digitaldemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Digitaldemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Digitaldemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/randb/digitaldemo1/Digitaldemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randb.digitaldemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Digitaldemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
